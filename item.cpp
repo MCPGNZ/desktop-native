8fa19f6017a1392a96a28b7b68d3aff915e320cb
mcpgnz <daniel.wojnarowski@moonlit.games>
2021-01-28 17:30:31 +0100

    feat(item): added icon, tooltip setters

--- item.cpp
+++ item.cpp
@@ -1,1 +1,25 @@
 #include "item.h"
+#include <Shlobj.h>
+#include <string>
+
+/* https://stackoverflow.com/questions/16945892/custom-folder-icons-with-desktop-ini-instant-refreshing/19440996#19440996 */
+void set_icon(LPWSTR absolute_path, LPWSTR iconPath)
+{
+    SHFOLDERCUSTOMSETTINGS fcs = {0};
+    fcs.dwSize = sizeof(SHFOLDERCUSTOMSETTINGS);
+    fcs.dwMask = FCSM_LOGO | FCSM_ICONFILE;
+    fcs.pszIconFile = iconPath;
+    fcs.cchIconFile = 0;
+    fcs.iIconIndex = 0;
+    SHGetSetFolderCustomSettings(&fcs, absolute_path, FCS_FORCEWRITE);
+}
+
+void set_tooltip(LPWSTR absolute_path, LPWSTR tooltip)
+{
+    SHFOLDERCUSTOMSETTINGS fcs = {0};
+    fcs.dwSize = sizeof(SHFOLDERCUSTOMSETTINGS);
+    fcs.dwMask = FCSM_INFOTIP;
+    fcs.pszInfoTip = tooltip;
+    fcs.cchInfoTip = 0;
+    SHGetSetFolderCustomSettings(&fcs, absolute_path, FCS_FORCEWRITE);
+}
--- item.h
+++ item.h
@@ -1,7 +1,11 @@
 #pragma once
+#include <wtypes.h>
 
 #ifdef DESKTOP_FRAMEWORK_EXPORTS
 #define DESKTOP_FRAMEWORK_API __declspec(dllexport)
 #else
 #define DESKTOP_FRAMEWORK_API __declspec(dllimport)
 #endif
+
+extern "C" DESKTOP_FRAMEWORK_API  void set_icon(LPWSTR absolute_path, LPWSTR iconPath);
+extern "C" DESKTOP_FRAMEWORK_API  void set_tooltip(LPWSTR absolute_path, LPWSTR tooltip);
